f11e371042b14bdefd6fead6f7868c9cc82bb4c2
PiotrO <piotro3425@gmail.com>
2021-10-04 13:03:15 +0200

    Working version without edge case support

--- OneLineUnrecognizedRemover/Program.cs
+++ OneLineUnrecognizedRemover/Program.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using System.IO;
+using System.Linq;
 
 namespace OneLineUnrecognizedRemover
 {
    class Program
    {
-      static void Main(string[] args) { }
+      static void Main(string[] args) => File.WriteAllLines("out\\" + args[0], File.ReadAllLines(args[0]).Where(e => !e.Contains("#unrecognized text#")).ToArray());
    }
 }
