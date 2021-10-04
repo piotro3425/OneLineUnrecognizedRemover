13e1a4beaab0cba29434791b8f48eb162d477b23
PiotrO <piotro3425@gmail.com>
2021-10-04 14:14:32 +0200

    Add edge case support - program parameter validity and out directory existence

--- OneLineUnrecognizedRemover/Program.cs
+++ OneLineUnrecognizedRemover/Program.cs
@@ -1,10 +1,12 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace OneLineUnrecognizedRemover
 {
    class Program
    {
-      static void Main(string[] args) => File.WriteAllLines("out\\" + args[0], File.ReadAllLines(args[0]).Where(e => !e.Contains("#unrecognized text#")).ToArray());
+      static async Task Main(string[] args) => await ((args.Length > 0 && File.Exists(args[0]) && Directory.Exists("out")) ? Task.Factory.StartNew(() => File.WriteAllLines("out\\" + args[0], File.ReadAllLines(args[0]).Where(e => !e.Contains("#unrecognized text#")).ToArray())).ConfigureAwait(true) : Task.Factory.StartNew(() => Console.WriteLine("Incorrect parameters")).ConfigureAwait(true));
    }
 }
